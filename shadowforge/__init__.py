86350dcb63c03713fadf422f76cd23040d49d634
Gitersg <shrinjoyspe21@gmail.com>
2026-06-30 10:34:47 +0530

    Add author metadata to package

--- shadowforge/__init__.py
+++ shadowforge/__init__.py
@@ -1,4 +1,6 @@
 """ShadowForge — Local Multi-Agent Desktop Automation System."""
 
 __version__ = "2.1.0"
-__author__ = "ShadowForge Team"
+__author__ = "Shrinjoy Ghosh"
+__copyright__ = "Copyright (c) 2026 Shrinjoy Ghosh"
+__license__ = "MIT"
